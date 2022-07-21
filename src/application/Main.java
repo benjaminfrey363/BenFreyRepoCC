dab6604528e17b5026b4e351232b52c938097fae
benjamin.frey <benjamin.frey@IC-CS233-25.uc.ucalgary.ca>
2022-07-21 16:54:08 -0600

    Added my branch label.

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,7 +3,6 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
@@ -14,6 +13,7 @@ public class Main extends Application {
 		try {
 			VBox root = new VBox();
 			root.getChildren().add(new Label("Brendan's Label"));
+			root.getChildren().add(new Label("Ben's Branch"));
 			Scene scene = new Scene(root,400,400);
 			// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
